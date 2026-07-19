5e8fbe0012f25b2f7dfb65ed98207b4ef4bb761b
Zee <zee@Williams-MacBook-Air.local>
2026-07-19 07:19:53 +0700

    redirect page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://muhammadazmi.web.id',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
